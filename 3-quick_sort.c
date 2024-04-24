09b8e53af3150830215cbd7eec4941686a59dab0
be-great <begreat5333@gmail.com>
2024-04-25 01:41:28 +0300

    task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,40 @@
 #include "sort.h"
 
+/**
+* swap - swap two elemant a, b
+* @a: the first element
+* @b: the second element
+*/
+void swap(int *a, int *b)
+{
+	int temp;
 
+	temp = *a;
+	*a = *b;
+	*b = temp;
+
+}
+/**
+* partition - partition the tree
+* @arr: the array
+* @low: the low partition
+* @high: the high partition
+* Return: the partition return
+*/
+int partition(int *arr, int low, int high)
+{
+
+}
+/**
+* tree - divide and conquer
+* @arr: the array
+* @low: the low partition
+* @high: the high partition
+*/
+void tree(int arr[], int low, int high)
+{
+
+}
 /**
 * quick_sort -  sorting algorithm for sorting large datasets
 * @array: the array
@@ -8,5 +42,5 @@
 */
 void quick_sort(int *array, size_t size)
 {
-
+	tree(array, 0, size - 1);
 }
