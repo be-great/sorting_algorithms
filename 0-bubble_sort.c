6bb71f1b9ecb871c857b92f34d643c5dfcc13df9
yasermoamd <yasermoamd@hotmail.com>
2024-04-28 01:06:16 -0700

    fix: bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,40 +1,28 @@
 #include "sort.h"
-/**
-* swap - swap two elemant a, b
-* @a: the first element
-* @b: the second element
-*/
-void swap(int *a, int *b)
-{
-	int temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-
-}
 
 /**
-* bubble_sort -  sorting algorithm for small sets of elements
-* @array: the array
-* @size: the size of the array
-*/
+ * bubble_sort - sort list with bubble
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t n = 0, t = size;
+	int holder;
 
-	if (array == NULL || sie < 2)
+	if (array == NULL)
 		return;
-	for (i = size; i > 0; i--)
+	for (t = size; t > 0; t--)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (n = 0; n < size - 1; n++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[n] > array[n + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				holder = array[n];
+				array[n] = array[n + 1];
+				array[n + 1] = holder;
 				print_array(array, size);
 			}
 		}
-
 	}
 }
