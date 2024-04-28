50ff37da22833ce430f6a9cb6437191f7bf3a0e6
yasermoamd <yasermoamd@hotmail.com>
2024-04-28 00:48:05 -0700

    print array of integer and header

--- sort.h
+++ sort.h
@@ -14,7 +14,7 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
-
+void print_array(const int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
