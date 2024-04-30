da87a6767d77202a0f2f82ec4df06cb7ab077f3b
be-great <begreat5333@gmail.com>
2024-04-30 03:39:47 +0300

    task 107, 7

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,84 @@
+#include "sort.h"
+
+/**
+* swap - swap two elemant a, b
+* @a: the first element
+* @b: the second element
+*/
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+
+}
+/**
+* partition - partition the tree
+* @arr: the array
+* @low: the low partition
+* @high: the high partition
+* @size: the size of the orignal array
+* Return: the partition return
+*/
+int partition(int *arr, int low, int high, int size)
+{
+	/** you want to but the pivot position in 'i' + 1*/
+	/* meaning :  i < pivot < i + 2*/
+	int pivot, i, j;
+
+	pivot = arr[high]; /* the last element*/
+	i = low; /*init left index*/
+	j = high;   /*init right index*/
+
+	while (1)
+	{
+		/* find in the left side a element greater than the pivot*/
+		while (arr[i] < pivot)
+		{
+			i++;
+		}
+		/* find in the right side a element small than the pivot*/
+		while (arr[j] > pivot)
+		{
+			j--;
+		}
+		if (i >= j)
+			return (j);
+		swap(&arr[i], &arr[j]);
+		print_array(arr, size);
+	}
+
+}
+/**
+* tree - divide and conquer
+* @arr: the array
+* @low: the low partition
+* @high: the high partition
+* @size: the size the array
+*/
+void tree(int arr[], int low, int high, int size)
+{
+	if (low < high)
+	{
+		int pi;
+
+		pi = partition(arr, low, high, size);
+		tree(arr, low, pi - 1, size);
+		tree(arr, pi + 1, high, size);
+	}
+}
+/**
+* quick_sort_hoare -  sorting algorithm for sorting large datasets
+* @array: the array
+* @size: the size of the array
+*/
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	tree(array, 0, size - 1, size);
+}
--- sort.h
+++ sort.h
@@ -25,4 +25,5 @@ void quick_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 #endif
