eabc35e2c18d37de497a8d4cde17def53dfe5291
yasermoamd <yasermoamd@hotmail.com>
2024-04-30 12:10:55 -0700

    header file

--- sort.h
+++ sort.h
@@ -27,7 +27,7 @@ void shell_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-
-
+void selection_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif
